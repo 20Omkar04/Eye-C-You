6b1df88ac5fb1de4f43313a337bc3b7549b3cd24
Omkar <114669316+20Omkar04@users.noreply.github.com>
2026-05-18 17:11:47 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+import BiometricDashboard from "./components/BiometricDashboard.jsx";
+export default function App() {
+  return (
+    <div className="bg-slate-950 min-h-screen">
+      <BiometricDashboard/>
+    </div>
+  );
+}
